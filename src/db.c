db50d4aac78b7de6a50ea6874ef57283c739127b
t00361654 <tanzhuxiaqiu@huawei.com>
2019-04-28 16:28:16 +0800

    Add a simple shell, could read input and exit.

--- src/db.c
+++ src/db.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
 
-
-int main(int argc, const char *argv[])
+void main(int argc, const char *argv[])
 {
-    printf("Hello, SimpleDB!\n");
-    return 0;
+    char *buffer;
+    size_t bufsize = 32;
+    buffer = (char *)malloc(sizeof(char) * bufsize);
+
+    while (1)
+    {
+        printf("sdb > ");
+        ssize_t bytes_read = getline(&buffer, &bufsize, stdin);
+        buffer[bytes_read - 1] = 0;
+        if (strcmp(buffer, ".exit") == 0)
+        {
+            exit(0);
+        }
+        else
+        {
+            printf("Unreconized command '%s'.\n", buffer);
+        }
+    }
 }
