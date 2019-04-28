2d29cb98a1f86f92c55a4b54db89ec145087b751
t00361654 <tanzhuxiaqiu@huawei.com>
2019-04-28 16:37:02 +0800

    Refactor, add new struct InputBuffer.

--- src/db.c
+++ src/db.c
@@ -3,24 +3,38 @@
 #include <string.h>
 #include <sys/types.h>
 
-void main(int argc, const char *argv[])
+typedef struct InputBuffer
 {
     char *buffer;
-    size_t bufsize = 32;
-    buffer = (char *)malloc(sizeof(char) * bufsize);
+    size_t buf_size;
+    ssize_t input_len;
+} InputBuffer;
+
+InputBuffer *new_input_buffer()
+{
+    InputBuffer *input_buffer = malloc(sizeof(InputBuffer));
+    input_buffer->buffer = NULL;
+    input_buffer->buf_size = 0;
+    input_buffer->input_len = 0;
+    return input_buffer;
+}
+
+void main(int argc, const char *argv[])
+{
+    InputBuffer *input_buffer = new_input_buffer();
 
     while (1)
     {
         printf("sdb > ");
-        ssize_t bytes_read = getline(&buffer, &bufsize, stdin);
-        buffer[bytes_read - 1] = 0;
-        if (strcmp(buffer, ".exit") == 0)
+        ssize_t bytes_read = getline(&input_buffer->buffer, &input_buffer->buf_size, stdin);
+        input_buffer->buffer[bytes_read - 1] = 0;
+        if (strcmp(input_buffer->buffer, ".exit") == 0)
         {
             exit(0);
         }
         else
         {
-            printf("Unreconized command '%s'.\n", buffer);
+            printf("Unreconized command '%s'.\n", input_buffer->buffer);
         }
     }
 }
