5143dcd2b21dd5b96511b7f94c6f7e686be05a74
shaqsnake <shaqsnake@gmail.com>
2019-04-30 11:27:35 +0800

    Add struct Table.

--- src/db.h
+++ src/db.h
@@ -1,7 +1,7 @@
 #define COLUMN_USERNAME_SIZE 32
 #define COLUMN_EMAIL_SIZE 255
 
-typedef struct InputBuffer
+typedef struct InputBuffer_t
 {
     char *buffer;
     size_t buf_size;
@@ -27,14 +27,14 @@ typedef enum
     PREPARE_SYSTAX_ERROR
 } PrepareResult;
 
-typedef struct Row
+typedef struct Row_t
 {
     uint32_t id;
     char username[COLUMN_USERNAME_SIZE];
     char email[COLUMN_EMAIL_SIZE];
 } Row;
 
-typedef struct Statement
+typedef struct Statement_t
 {
     StatementType type;
     Row row_to_insert;
@@ -52,3 +52,15 @@ typedef struct Statement
 const uint32_t USERNAME_OFFSET = ID_OFFSET + ID_SIZE;
 const uint32_t EMAIL_OFFSET = ID_OFFSET + ID_SIZE + USERNAME_SIZE;
 const uint32_t ROW_SIZE = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE;
+
+#define PAGE_SIZE 4096
+// const uint32_t ROWS_PER_PAGE = PAGE_SIZE / ROW_SIZE;
+const uint32_t ROWS_PER_PAGE = PAGE_SIZE / (ID_SIZE + USERNAME_SIZE + EMAIL_SIZE);
+#define TABLE_MAX_PAGES 100
+// const uint32_t TABLE_MAX_ROWS = ROWS_PER_PAGE * TABLE_MAX_PAGES;
+const uint32_t TABLE_MAX_ROWS = PAGE_SIZE / (ID_SIZE + USERNAME_SIZE + EMAIL_SIZE) * TABLE_MAX_PAGES;
+
+typedef struct Table_t{
+    uint32_t num_rows;
+    void * pages[TABLE_MAX_PAGES];
+} Table;
