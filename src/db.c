ea2b07e65f9c7f0e7dfba30383863df198742a3b
shaqsnake <shaqsnake@gmail.com>
2019-04-30 11:53:27 +0800

    Add func row_slot which can locate the page.

--- src/db.c
+++ src/db.c
@@ -96,6 +96,18 @@ void deserialize_row(void *src, Row *dest)
     memcpy(&(dest->email), src + EMAIL_OFFSET, EMAIL_SIZE);
 }
 
+void *row_slot(Table *table, uint32_t row_num)
+{
+    uint32_t page_num = row_num / ROWS_PER_PAGE;
+    void *page = table->pages[page_num];
+    if (page == NULL) {
+        page = table->pages[page_num] = malloc(PAGE_SIZE);
+    }
+    uint32_t row_offset = row_num % ROWS_PER_PAGE;
+    uint32_t byte_offset = row_offset * ROW_SIZE;
+    return page + byte_offset;
+}
+
 void main(int argc, const char *argv[])
 {
     InputBuffer *input_buffer = new_input_buffer();
