e05ffd6bc4958c96b4152b52bb106539241794ce
shaqsnake <shaqsnake@gmail.com>
2019-04-30 11:15:28 +0800

    Add serialize_row and deserialize_row.

--- src/db.c
+++ src/db.c
@@ -82,6 +82,20 @@ void execute_statement(Statement *statement)
     }
 }
 
+void serialize_row(Row *src, void *dest)
+{
+    memcpy(dest + ID_OFFSET, &(src->id), ID_SIZE);
+    memcpy(dest + USERNAME_OFFSET, &(src->username), USERNAME_SIZE);
+    memcpy(dest + EMAIL_OFFSET, &(src->email), EMAIL_SIZE);
+}
+
+void deserialize_row(void *src, Row *dest)
+{
+    memcpy(&(dest->id), src + ID_OFFSET, ID_SIZE);
+    memcpy(&(dest->username), src + USERNAME_OFFSET, USERNAME_SIZE);
+    memcpy(&(dest->email), src + EMAIL_OFFSET, EMAIL_SIZE);
+}
+
 void main(int argc, const char *argv[])
 {
     InputBuffer *input_buffer = new_input_buffer();
