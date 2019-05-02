ad50eca4d0105b8bc849b7387befbd0fb232aa35
Sean Tan <shaqsnake@gmail.com>
2019-05-02 10:36:34 +0800

    Fixed header, change define to const variables.

--- src/db.h
+++ src/db.h
@@ -47,24 +47,18 @@ typedef struct Statement_t
 } Statement;
 
 #define size_of_attribute(Struct, Attribute) sizeof(((Struct *)0)->Attribute)
-// const uint32_t ID_SIZE = size_of_attribute(Row, id);
-// const uint32_t USERNAME_SIZE = size_of_attribute(Row, username);
-// const uint32_t EMAIL_SIZE = size_of_attribute(Row, email);
-// const uint32_t ID_OFFSET = 0;
-#define ID_SIZE size_of_attribute(Row, id)
-#define USERNAME_SIZE size_of_attribute(Row, username)
-#define EMAIL_SIZE size_of_attribute(Row, email)
-#define ID_OFFSET 0
+const uint32_t ID_SIZE = size_of_attribute(Row, id);
+const uint32_t USERNAME_SIZE = size_of_attribute(Row, username);
+const uint32_t EMAIL_SIZE = size_of_attribute(Row, email);
+const uint32_t ID_OFFSET = 0;
 const uint32_t USERNAME_OFFSET = ID_OFFSET + ID_SIZE;
-const uint32_t EMAIL_OFFSET = ID_OFFSET + ID_SIZE + USERNAME_SIZE;
+const uint32_t EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE;
 const uint32_t ROW_SIZE = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE;
 
 #define PAGE_SIZE 4096
-// const uint32_t ROWS_PER_PAGE = PAGE_SIZE / ROW_SIZE;
-const uint32_t ROWS_PER_PAGE = PAGE_SIZE / (ID_SIZE + USERNAME_SIZE + EMAIL_SIZE);
+const uint32_t ROWS_PER_PAGE = PAGE_SIZE / ROW_SIZE;
 #define TABLE_MAX_PAGES 100
-// const uint32_t TABLE_MAX_ROWS = ROWS_PER_PAGE * TABLE_MAX_PAGES;
-const uint32_t TABLE_MAX_ROWS = PAGE_SIZE / (ID_SIZE + USERNAME_SIZE + EMAIL_SIZE) * TABLE_MAX_PAGES;
+const uint32_t TABLE_MAX_ROWS = ROWS_PER_PAGE * TABLE_MAX_PAGES;
 
 typedef struct Table_t
 {
